fcfc8d5d84e053ed24f549d7bc02cd5b68f0cf53
Yash Singhal <yash.24bce10839@vitbhopal.ac.in>
2025-08-22 19:23:10 +0530

    Solution for Day-07

--- Day07.cpp
+++ Day07.cpp
@@ -35,22 +35,3 @@ int main() {
     }
     return 0;
 }
-
-
-
-
-                                                   // DAY 07 - "WATERMELON(4A)"
-
-#include <iostream>
-using namespace std;
-int main() {
-    int w;
-    cin >> w;
-    if (w > 2 && w % 2 == 0) {
-        cout << "YES" << endl;
-    }
-    else {
-        cout << "NO" << endl;
-    }
-    return 0;
-}
